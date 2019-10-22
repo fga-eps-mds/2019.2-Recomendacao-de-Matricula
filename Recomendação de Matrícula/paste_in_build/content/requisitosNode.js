d383a604fca91c033512c1d49ea011beb4ce4361
thiagomes <thiago.luiz20000@gmail.com>
2019-10-22 12:29:45 -0300

    Recolhendo os pre-requesitos de cada materia e configurando o browserify
    closes #24,#25
    
    Co-authored-by wagnermc506 <wagnermc506@gmail.com>

--- Recomendação de Matrícula/paste_in_build/content/requisitosNode.js
+++ Recomendação de Matrícula/paste_in_build/content/requisitosNode.js
@@ -1,41 +1,25 @@
 const request = require('request');
 const cheerio = require('cheerio');
-let pre = ""
-request('https://matriculaweb.unb.br/graduacao/disciplina.aspx?cod=193674', (error, response, html) => {
-    if(!error && response.statusCode == 200){
-        let $ = cheerio.load(html);
-        //console.log($("document"))
-        let requisitos = $('#datatable tbody tr').eq(5)
-        //let a = requisitos.children('td').children('strong')
-        //let b = requisitos.children('td').children('a').children('strong')
-        requisitos.children('td').children().each((i, el) => {
-            //console.log(el.tagName)
-            if(el.tagName == 'a'){
-                let a = $(el).children('strong').text().trim()
-                console.log(a)
+let pre = ""             
+chrome.storage.local.get(["materiasR"], function(result){  
+    for(codigo of result.materiasR){                                           //193674
+        request('https://matriculaweb.unb.br/graduacao/disciplina.aspx?cod='.concat(codigo), (error, response, html) => {
+            if(!error && response.statusCode == 200){
+                let $ = cheerio.load(html)
+
+                let requisitos = $('#datatable tbody tr').eq(5)
+                
+                requisitos.children('td').children().each((i, el) => {
+                    if(el.tagName == 'a'){
+                        let a = $(el).children('strong').text().trim()
+                        console.log(a)
+                    }
+                    else if(el.tagName == 'strong'){
+                        let b = $(el).text().trim()
+                        //console.log(b)
+                    }
+                })
             }
-            else if(el.tagName == 'strong'){
-                let b = $(el).text().trim()
-                console.log(b)
-            }
-            /*if($(el).children('a')){
-                codigo[i] = $(el).children('a').children('strong').text()
-                let logic = $(el).children('strong').text()               
-            }*/
         })
     }
-});
-
-
-
-/*var materias
-
-chrome.storage.local.get(["materiasR"], function(result){ 
-    //materias = result.materiasR
-    for(codigo of result.materiasR){
-        $.get( "https://matriculaweb.unb.br/graduacao/disciplina.aspx?cod=".concat(codigo), function( data ) {
-            console.log(data)
-        });
-    }
 })
-*/
--- Recomendação de Matrícula/src/src/App.js
+++ Recomendação de Matrícula/src/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Conteudo from './components/conteudo';
 import Titulo from './components/title'
+import '/build/background/background.js'; 
 
 function App() {
+  
+  const[isMatriculaWeb, setisMatriculaWeb ] = useState([]);
+
+  useEffect(()=>{
+ 
+
+  },[]);
 
   return (
   <Box width={300} height={450} bgcolor='grey.300'>     
     <Titulo/>
     <Conteudo/>
+   
   </Box>  
   );
 }
