fc2a2c4864217470c7fdb496bcbd57bd88fb4d4c
fmaachadoo <filipe__machado@hotmail.com>
2019-10-25 19:01:39 -0300

    Adicionado script que automatiza a produção da extensão, resolvendo assim o problema da paste_in_build

---
+++ Recomendação de Matrícula/build/precache-manifest.d20f0aa1fef9654b0baa8076b436714f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cd91dd82e4c95e59ea4d0bfacecae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469139948a86b40a1214",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0fe6fdf5b6fc20dffdb5",
+    "url": "/static/js/2.bcba2f33.chunk.js"
+  },
+  {
+    "revision": "469139948a86b40a1214",
+    "url": "/static/js/main.8e254403.chunk.js"
+  },
+  {
+    "revision": "89dce495c809847ec7c0",
+    "url": "/static/js/runtime-main.6acb86cc.js"
+  }
+]);
