6f2a06d2e9154ab2aec1f3a4bcd397aefe045a97
Matheus Azevedo <matheus.klementt@gmail.com>
2019-10-25 18:02:49 -0300

    Recolhe corretamente as materias e mencoes do historico
    Co-authored-by: lonaxi <lucaslonaxi@gmail.com>

--- Recomendação de Matrícula/paste_in_build/content/historicoescolar.js
+++ Recomendação de Matrícula/paste_in_build/content/historicoescolar.js
@@ -1,8 +1,35 @@
 var elements = document.querySelectorAll("tr");
+let indexCodigoMateria = 0
+let indexNomeMateria = 1
+let indexMencao = 3
+
+var materias = []
+var codigo, nomeMateria, mencao;
 
 for (element of elements) {
-    console.log(element.textContent[0]);
+    
     if (!isNaN(parseInt(element.textContent[0]))) { //Seleciona apenas as linhas da tabela de materias
-        element.style['background-color'] = "#ceffca";
+        codigo = element.children[indexCodigoMateria].innerText;
+        nomeMateria = element.children[indexNomeMateria].innerText;
+        mencao = element.children[indexMencao].innerText;
+
+        var jaCursada = false;
+
+        for (materia of materias) {
+            if (codigo == materia.codigo) {
+                materia.mencoes.push(mencao);
+                jaCursada = true;
+                break;
+            }
+        }
+
+        if (!jaCursada) {
+            materias.push({ 
+                codigo,
+                nomeMateria,
+                mencoes : [mencao]
+            });
+        }
+        
     }
 }
