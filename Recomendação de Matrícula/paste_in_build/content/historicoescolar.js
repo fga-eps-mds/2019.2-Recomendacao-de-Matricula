61f03af9acf754321c96ce48aef3e570814f05b1
Matheus Azevedo <matheus.klementt@gmail.com>
2019-10-26 17:24:40 -0300

    Calculando número de reprovacoes
    Co-authored-by: lonaxi <lucaslonaxi@gmail.com>

--- Recomendação de Matrícula/paste_in_build/content/historicoescolar.js
+++ Recomendação de Matrícula/paste_in_build/content/historicoescolar.js
@@ -14,10 +14,16 @@ for (element of elements) {
         mencao = element.children[indexMencao].innerText;
 
         var jaCursada = false;
+        var reprovacoes = 0;
+
+        if (mencao == "SR" || mencao == "II" || mencao == "MI") { //Verifica se o usuario reprovou
+            reprovacoes = 1;
+        }
 
         for (materia of materias) {
-            if (codigo == materia.codigo) {
+            if (codigo == materia.codigo) { //Verifica se a materia ja foi cursada
                 materia.mencoes.push(mencao);
+                materia.reprovacoes += reprovacoes;
                 jaCursada = true;
                 break;
             }
@@ -27,6 +33,7 @@ for (element of elements) {
             materias.push({ 
                 codigo,
                 nomeMateria,
+                reprovacoes,
                 mencoes : [mencao]
             });
         }
