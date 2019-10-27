6908fb762eb1b3185ed27c449d764266e1135b7c
Matheus Azevedo <matheus.klementt@gmail.com>
2019-10-27 18:34:08 -0300

    Salva materias do historico no chrome storage
    Co-authored-by: lonaxi <lucaslonaxi@gmail.com>

--- Recomendação de Matrícula/scripts/content/historicoescolar.js
+++ Recomendação de Matrícula/scripts/content/historicoescolar.js
@@ -4,39 +4,66 @@ let indexNomeMateria = 1
 let indexMencao = 3
 
 var materias = []
-var codigo, nomeMateria, mencao;
+var codigo, nomeMateria, aprovado, reprovacoes, mencao;
 
-for (element of elements) {
-    
-    if (!isNaN(parseInt(element.textContent[0]))) { //Seleciona apenas as linhas da tabela de materias
-        codigo = element.children[indexCodigoMateria].innerText;
-        nomeMateria = element.children[indexNomeMateria].innerText;
-        mencao = element.children[indexMencao].innerText;
+chrome.storage.local.get(["materiasHistorico"], function(result){
+    if(result.materiasHistorico == null){
+        leituraFeita = false;
+    }
+    else{
+        leituraFeita = true;
+    }
+    console.log(leituraFeita)
 
-        var jaCursada = false;
-        var reprovacoes = 0;
+    if(leituraFeita == false){
+        for (element of elements) {
+            if (!isNaN(parseInt(element.textContent[0]))) { //Seleciona apenas as linhas da tabela de materias
+                codigo = element.children[indexCodigoMateria].innerText;
+                nomeMateria = element.children[indexNomeMateria].innerText;
+                mencao = element.children[indexMencao].innerText;
 
-        if (mencao == "SR" || mencao == "II" || mencao == "MI") { //Verifica se o usuario reprovou
-            reprovacoes = 1;
-        }
+                var jaCursada = false;
+                aprovado = false;
+                reprovacoes = 0;
 
-        for (materia of materias) {
-            if (codigo == materia.codigo) { //Verifica se a materia ja foi cursada
-                materia.mencoes.push(mencao);
-                materia.reprovacoes += reprovacoes;
-                jaCursada = true;
-                break;
+                if (mencao == "SR" || mencao == "II" || mencao == "MI") { //Verifica se o usuario reprovou
+                    reprovacoes = 1;
+                } else if (mencao == "MM" || mencao == "MS" || mencao == "SS") {
+                    aprovado = true;
+                }
+
+                for (materia of materias) {
+                    if (codigo == materia.codigo) { //Verifica se a materia ja foi cursada
+                        materia.mencoes.push(mencao);
+                        materia.reprovacoes += reprovacoes;
+                        jaCursada = true;
+                        break;
+                    }
+                }
+
+                if (!jaCursada) {
+                    materias.push({ 
+                        codigo,
+                        nomeMateria,
+                        aprovado,
+                        reprovacoes,
+                        mencoes : [mencao]
+                    });
+                }   
             }
         }
+        chrome.storage.local.set({
+            materiasHistorico : materias
+        }, function(result){
+            console.log(materias.length, "materias armazenadas");
+        })
+    }
+})
 
-        if (!jaCursada) {
-            materias.push({ 
-                codigo,
-                nomeMateria,
-                reprovacoes,
-                mencoes : [mencao]
-            });
-        }
-        
+for (materia of materias) {
+    if (!materia.aprovado && materia.reprovacoes >= 2) {
+        console.log("RISCO DE DESLIGAMENTO")
+        console.log(materia.nomeMateria)
     }
 }
+
