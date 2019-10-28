9b4c47e80346af74137b3fb737efdaa758e1c16b
thiagomes <thiago.luiz20000@gmail.com>
2019-10-28 13:48:54 -0300

    Organização dos pré-requisitos das matérias, separando os vetores de forma lógica
    Co-authored-by: wagnermc506 <wagnermc506@gmail.com>

--- Recomendação de Matrícula/paste_in_build/content/requisitosNode.js
+++ Recomendação de Matrícula/paste_in_build/content/requisitosNode.js
@@ -1,35 +1,57 @@
 const request = require('request');
 const cheerio = require('cheerio');
-let pre = ""
 let materiasNaoCursadas = []
-let teste = []
-chrome.storage.local.get(["materias"], function(result){
-    for(materia of result.materias){
-        if(!materia.aprovado){
-            materiasNaoCursadas.push(materia.codigo);
-        }
-    }  
-    for(codigo of materiasNaoCursadas){                                           //193674
-        request('https://matriculaweb.unb.br/graduacao/disciplina.aspx?cod='.concat(codigo), (error, response, html) => {
-            if(!error && response.statusCode == 200){
-                let $ = cheerio.load(html)
 
+chrome.storage.local.get(["materias"], function(result){
+    //console.log(result.materias);
+    let materias = result.materias;
+    for(i = 0; i < result.materias.length; i++){
+        if(!result.materias[i].aprovado){
+            materiasNaoCursadas.push([i, result.materias[i].codigo]);
+        }
+    }
+
+    for(i = 0; i < materiasNaoCursadas.length; i++){
+        request('https://matriculaweb.unb.br/graduacao/disciplina.aspx?cod='.concat(materiasNaoCursadas[i][1]), (error, response, html) => {
+            if(!error && response.statusCode == 200){
+                let $ = cheerio.load(html) 
                 let requisitos = $('#datatable tbody tr').eq(5)
-                
-                requisitos.children('td').children().each((i, el) => {
+                let vetorGeral = []
+                requisitos.children('td').children().each((iterador, el) => {
                     if(el.tagName == 'a'){
-                        let a = $(el).children('strong').text().trim()
-                        teste.push(a);
+                        let a
+                        a = $(el).children('strong').text().trim()
+                        vetorGeral.push(a);
+                        //console.log(a);
                     }
-                    else if(el.tagName == 'strong'){
+                    if(el.tagName == 'strong'){
                         let b = $(el).text().trim()
+                        vetorGeral.push(b)
                         //console.log(b)
                     }
-                    
                 })
+                //Implemetações para o vetorGeral
+                let vetorFinal = []
+                for(j = 0, vetorTemp = []; j < vetorGeral.length; j++){
+                    if(j == 0){
+                        vetorTemp.push(vetorGeral[j])
+                    }
+                    else if(vetorGeral[j] == "E"){
+                        if(!vetorTemp.includes(vetorGeral[j-1])){
+                            vetorTemp.push(vetorGeral[j-1]);
+                        }
+                        vetorTemp.push(vetorGeral[j+1]);
+                    }
+                    else if(vetorGeral[j] == "OU" || vetorGeral[j] == ""){
+                        if(vetorTemp.length == 0){
+                            vetorTemp.push(vetorGeral[j-1])
+                        }
+                        vetorFinal.push(vetorTemp);
+                        vetorTemp = []
+                    }
+                }
+                //console.log(vetorFinal)
             }
         })
     }
 })
-
-console.log(teste)
