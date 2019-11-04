60d66856600dd4e820b72e4529725b7d12bd1651
wagnermc506 <wagnermc506@gmail.com>
2019-11-04 18:36:30 -0300

    Mudanças no destaque para destacar materias as quais os pré-requisitos não foram atendidos

--- Recomendação de Matrícula/scripts/content/requisitos.js
+++ Recomendação de Matrícula/scripts/content/requisitos.js
@@ -69730,77 +69730,79 @@ const request = require('request');
 const cheerio = require('cheerio');
 let materiasCursadas = []
 let materiasNaoCursadas = []
-chrome.storage.local.get(["materias"], function(result){
-    //console.log(result.materias);
-    let materias = result.materias;
-    for(i = 0; i < result.materias.length; i++){
-        if(!result.materias[i].aprovado){
-            materiasNaoCursadas.push([i, result.materias[i].codigo]);
-        }
-        else{
-            materiasCursadas.push(result.materias[i].codigo);
-        }
-    }
-
-    for(i = 0; i < materiasNaoCursadas.length; i++){
-        request('https://matriculaweb.unb.br/graduacao/disciplina.aspx?cod='.concat(materiasNaoCursadas[i][1]), (error, response, html) => {
-            if(!error && response.statusCode == 200){
-                let $ = cheerio.load(html) 
-                let codigoAtual = $('#datatable tbody tr').eq(1).children('td').text().trim()
-                let requisitos = $('#datatable tbody tr').eq(5)
-                let vetorGeral = []
-                requisitos.children('td').children().each((iterador, el) => {
-                    if(el.tagName == 'a'){
-                        let a
-                        a = $(el).children('strong').text().trim()
-                        vetorGeral.push(a);
-                        //console.log(a);
-                    }
-                    if(el.tagName == 'strong'){
-                        let b = $(el).text().trim()
-                        vetorGeral.push(b)
-                        //console.log(b)
-                    }
-                })
-                //Implemetações para o vetorGeral
-                let vetorFinal = []
-                for(j = 0, vetorTemp = []; j < vetorGeral.length; j++){
-                    if(j == 0){
-                        vetorTemp.push(vetorGeral[j])
-                    }
-                    else if(vetorGeral[j] == "E"){
-                        if(!vetorTemp.includes(vetorGeral[j-1])){
-                            vetorTemp.push(vetorGeral[j-1]);
-                        }
-                        vetorTemp.push(vetorGeral[j+1]);
-                    }
-                    else if(vetorGeral[j] == "OU" || vetorGeral[j] == ""){
-                        if(vetorTemp.length == 0){
-                            vetorTemp.push(vetorGeral[j-1])
-                        }
-                        vetorFinal.push(vetorTemp);
-                        vetorTemp = []
-                    }
-                    else if(vetorGeral[j] == " "){
-                        vetorFinal.push(vetorTemp);
-                    }
-                }
-                for(j = 0; j < materias.length; j++){
-                    if(materias[j].codigo == codigoAtual){
-                        materias[j].requisitos = vetorFinal;
-                    }
-                }
-                avaliarSePodeSerFeita(materias);
-                chrome.storage.local.set({
-                    status : 2,
-                    materias : materias
-                }, function(result){
-                    console.log("Requisitos Salvos");
-                })
+chrome.storage.local.get(["materias", "status"], function(result){
+    if(result.status != 2){
+        //console.log(result.materias);
+        let materias = result.materias;
+        for(i = 0; i < result.materias.length; i++){
+            if(!result.materias[i].aprovado){
+                materiasNaoCursadas.push([i, result.materias[i].codigo]);
             }
-        })
+            else{
+                materiasCursadas.push(result.materias[i].codigo);
+            }
+        }
+
+        for(i = 0; i < materiasNaoCursadas.length; i++){
+            request('https://matriculaweb.unb.br/graduacao/disciplina.aspx?cod='.concat(materiasNaoCursadas[i][1]), (error, response, html) => {
+                if(!error && response.statusCode == 200){
+                    let $ = cheerio.load(html) 
+                    let codigoAtual = $('#datatable tbody tr').eq(1).children('td').text().trim()
+                    let requisitos = $('#datatable tbody tr').eq(5)
+                    let vetorGeral = []
+                    requisitos.children('td').children().each((iterador, el) => {
+                        if(el.tagName == 'a'){
+                            let a
+                            a = $(el).children('strong').text().trim()
+                            vetorGeral.push(a);
+                            //console.log(a);
+                        }
+                        if(el.tagName == 'strong'){
+                            let b = $(el).text().trim()
+                            vetorGeral.push(b)
+                            //console.log(b)
+                        }
+                    })
+                    //Implemetações para o vetorGeral
+                    let vetorFinal = []
+                    for(j = 0, vetorTemp = []; j < vetorGeral.length; j++){
+                        if(j == 0){
+                            vetorTemp.push(vetorGeral[j])
+                        }
+                        else if(vetorGeral[j] == "E"){
+                            if(!vetorTemp.includes(vetorGeral[j-1])){
+                                vetorTemp.push(vetorGeral[j-1]);
+                            }
+                            vetorTemp.push(vetorGeral[j+1]);
+                        }
+                        else if(vetorGeral[j] == "OU" || vetorGeral[j] == ""){
+                            if(vetorTemp.length == 0){
+                                vetorTemp.push(vetorGeral[j-1])
+                            }
+                            vetorFinal.push(vetorTemp);
+                            vetorTemp = []
+                        }
+                        else if(vetorGeral[j] == " "){
+                            vetorFinal.push(vetorTemp);
+                        }
+                    }
+                    for(j = 0; j < materias.length; j++){
+                        if(materias[j].codigo == codigoAtual){
+                            materias[j].requisitos = vetorFinal;
+                        }
+                    }
+                    avaliarSePodeSerFeita(materias);
+                    chrome.storage.local.set({
+                        status : 2,
+                        materias : materias
+                    }, function(result){
+                        console.log("Requisitos Salvos");
+                    })
+                }
+            })
+        }
+        //console.log(materias)
     }
-    //console.log(materias)
 })
 
 function avaliarSePodeSerFeita(index){
