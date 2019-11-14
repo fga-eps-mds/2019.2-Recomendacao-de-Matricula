0ba0a0b81b53d58ad0cd34ef0591ef99902463f8
thiagomes <thiago.luiz20000@gmail.com>
2019-11-14 17:04:01 -0300

    Implementando a função de destaque para a página do fluxo, para isso foi criado um novo arquivo destaquefluxo.js para destacar essa página que era diferente das anteriores já destacadas
    Closes #39
    
    Co-authored-by: wagnermc506 <wagnermc506@gmail.com>

--- Recomendação de Matrícula/scripts/content/destaquefluxo.js
+++ Recomendação de Matrícula/scripts/content/destaquefluxo.js
@@ -1,13 +1,15 @@
 let elements = document.querySelectorAll("tr"); //Seleciona as materias da tabela
-let orangeHex = "#ff00d6" //Cor de destaque para matérias que não podem ser cursadas ainda
+let purpleHex = "#ff00d6" //Cor de destaque para matérias que não podem ser cursadas ainda
 let greenHex = "#ceffca" //Cor de destaque de materias ja cursadas
 let yellowHex = "#ffff00" //Cor de destaque de materias nao cursadas
+let redHex = "#ff4000" //Cor de destaque para materias em risco de desligamento
 
-chrome.storage.local.get(["status", "materias"], function(result){
+chrome.storage.local.get(["status", "materias", "materiasHistorico"], function(result){
     if (result.status != null) {
         let materiasCursadas = []; //Materias já cursadas as quais o aluno já está aprovado ou fazendo
         let materiasRequisitosAtendidos = []; //Materias não cursadas que atendem aos pré-requisitos
         let materiasRequisitosNaoAtendidos = []; //Materias não cursadas que não atendem os pré-requisitos
+        let materiasEmCondicao = [] //Materias em risco de desligamento
 
         for (i = 0; i < result.materias.length; i++) {
             if (result.materias[i].aprovado) {
@@ -21,6 +23,12 @@ chrome.storage.local.get(["status", "materias"], function(result){
             }
         }
 
+        for (materia of result.materiasHistorico) {
+            if (!materia.aprovado && materia.reprovacoes >= 2) {
+                materiasEmCondicao.push(materia.codigo);
+            }
+        } 
+
         for(elt of elements) {
             let codigoDaMateria = elt.querySelectorAll("td")[3]
             if(codigoDaMateria){
@@ -30,7 +38,10 @@ chrome.storage.local.get(["status", "materias"], function(result){
                 } else if (materiasRequisitosAtendidos.includes(codigoDaMateria.innerText)) {
                     elt.style['background-color'] = yellowHex;
                 } else if (materiasRequisitosNaoAtendidos.includes(codigoDaMateria.innerText)) {
-                    elt.style['background-color'] = orangeHex;
+                    elt.style['background-color'] = purpleHex;
+                }
+                else if (materiasEmCondicao.includes(codigoDaMateria)){
+                    elt.style['background-color'] = redHex;
                 }
             }
         }
